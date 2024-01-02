045cb98c0324cdb61ad20c29de6bc89483ab65e0
Bart-acaisoft <bartosz.stawowy@acaisoft.com>
2024-01-02 11:00:32 +0100

    connection test

--- helpers/re_matches.py
+++ helpers/re_matches.py
@@ -6,8 +6,8 @@ def match_engine(mint, ore):
         return re.match(r'^(.*?)\s+(\d{4})?\s*-\s*([\d,]+)zł$', ore)
     if mint == 'silver_mint':
         patterns = [
-            re.compile(r'^(.*?) (\d{4}),.*? - (\d+,\d+\.\d{2})\xa0zł$'),  # Pattern with a comma in the year
-            re.compile(r'^(.*?) (\d{4}),.*? - (\d+\.\d{2})\xa0zł$'),  # Pattern without a comma in the year
+            re.compile(r'^(.*?) (\d{4}),.*? - (\d+,\d+\.\d{2})\xa0zł$'),
+            re.compile(r'^(.*?) (\d{4}),.*? - (\d+\.\d{2})\xa0zł$'),
         ]
         for pattern in patterns:
             return pattern.match(ore)
